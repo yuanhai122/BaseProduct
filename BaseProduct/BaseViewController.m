1010e20ca927432e440e4dd67d10da7651b9c9f4
WhaleSea <yuanhai@polysoft.com.cn>
2018-07-11 07:46:58 +0800

    BaseVC添加

--- BaseProduct/BaseViewController.m
+++ BaseProduct/BaseViewController.m
@@ -8,7 +8,9 @@
 
 #import "BaseViewController.h"
 
-@interface BaseViewController ()
+@interface BaseViewController (){
+    __weak UILabel * titleLabel;
+}
 
 @end
 
@@ -16,9 +18,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
 }
 
+#pragma mark 左侧返回按钮
+- (void)customBackBarButtonItem{
+    UIImage * image = [UIImage imageNamed:@""];
+    UIButton * button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 36, 36)];
+    [button setImage:image forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(backBtn:) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem * barBtn = [[UIBarButtonItem alloc] initWithCustomView:button];
+    if ([[UIDevice currentDevice].systemVersion doubleValue] >= 7.0) {
+        UIBarButtonItem * negativeSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
+        negativeSpace.width = -12;
+        self.navigationItem.leftBarButtonItems = @[negativeSpace,barBtn];
+    }else{
+        self.navigationItem.leftBarButtonItem = barBtn;
+    }
+}
+
+- (void)backBtn:(UIButton *)btn{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+#pragma mark 自定义title
+- (void)custonTitle:(NSString *)title{
+    if (titleLabel) {
+        [titleLabel removeFromSuperview];
+    }
+    UILabel * label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 44)];
+    label.text = title;
+    label.font = [UIFont boldSystemFontOfSize:17];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.textColor = [UIColor whiteColor];
+    titleLabel = label;
+    self.navigationItem.titleView = titleLabel;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
